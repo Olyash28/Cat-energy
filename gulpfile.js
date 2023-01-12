2c78b6a570ddedf9575f1bd554e258ce2552c881
olk <olyash28@gmail.com>
2023-01-12 19:54:36 +0300

    build done

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,51 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import sourcemap from 'gulp-sourcemaps';
+import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import broswerSync from 'browser-sync';
+import babel from 'gulp-babel';  //преобразует код для старых браузеров
+import concat from 'gulp-concat';  //для конкатенации (объединения) файлов js
+import uglify from 'gulp-uglify';  //Минимизирует JavaScript
+import clean from 'gulp-clean';
+import csso from 'gulp-csso';  //минимизирует css
+import rename from 'gulp-rename';  //переименовывает css в min.css
+import imagemin from 'gulp-imagemin';
+
+//сделать copy для html и fonts, favicon. что-то решить с min-css
+
+const sync = broswerSync.create();
+
+//img
+
+const images = () => {
+  return gulp.src('source/img/**/*.{jpg,png,svg}')
+    .pipe(imagemin())
+    .pipe(gulp.dest("build/img")) //куда кладем
+};
+
+// Scripts
+
+const cleanJs = () => {
+  return gulp.src("source/js/**/*.min.js", { read: false, allowEmpty: true })
+    .pipe(clean());
+};
+
+const scripts = () => {
+  return gulp.src("source/js/**/*.js", { sourcemaps: true }) //** -все, * -любое
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest("source/js"))
+    .pipe(gulp.dest("build/js"));
+};
+
+const cleanFunc = () => {
+  cleanJs();
+  return gulp.src('build', { read: false, allowEmpty: true })
+    .pipe(clean());
+};
 
 // Styles
 
@@ -16,12 +57,25 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
+};
+
+//копируем все в build
+
+const fonts = () => {
+  return gulp.src("source/fonts/**/*.{woff,woff2}")
+    .pipe(gulp.dest("build/fonts"))
 }
 
-exports.styles = styles;
+const favicons = () => {
+  return gulp.src("source/favicon/**/*")
+    .pipe(gulp.dest("build/favicon"))
+}
 
 // Server
 
@@ -37,19 +91,25 @@ const server = (done) => {
   done();
 }
 
-exports.server = server;
-
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch(["source/js/**/*.js", "!source/js/**/*.min.js"], gulp.series(cleanJs, scripts, sync.reload));
   gulp.watch("source/*.html").on("change",  sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+const html = () => {
+  return gulp.src("source/**/*.html")
+    .pipe(gulp.dest("build"))
+};
+
+const build = gulp.series(cleanFunc, images, gulp.parallel(styles, scripts, favicons, fonts, html));
+
+export {
+  build
+}
+
+export default gulp.series(
+  styles, scripts, server, watcher
 );
-
-const build = gulp.series(styles);
-
-exports.build = build;
--- source/js/menu.js
+++ source/js/menu.js
@@ -4,4 +4,5 @@ const menu = document.querySelector('.main-nav');
 burger.addEventListener('click', () => {
   menu.classList.toggle('main-nav--open');
   burger.classList.toggle('header-logo__button--close');
+  console.log(123)
 } )
