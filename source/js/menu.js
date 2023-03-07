816e8f4c4509d27f672faa4f76e83a0196186f98
Olk <new@Olk.local>
2023-03-07 12:53:25 +0300

    Update 2023-03-07T09:53:24.978Z

--- source/js/menu.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.header-logo__button');
-const menu = document.querySelector('.main-nav');
-
-burger.addEventListener('click', () => {
-  menu.classList.toggle('main-nav--open');
-  burger.classList.toggle('header-logo__button--close');
-} )
